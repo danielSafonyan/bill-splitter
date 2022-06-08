99b032f673ac925e51ff97b3fb59c4f60c4f8868
danielSafonyan <daniel.safonyan@gmail.com>
2022-06-08 17:03:42 +0100

    finished the project!

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,25 +1,76 @@
-def take_input():
+import sys
+import random
+
+
+def get_num_friends():
     while True:
         print('Enter the number of friends joining (including you): ')
-        friends_joining = input()
+        num_friends_joining = input()
 
         try:
-            friends_joining = int(friends_joining)
+            num_friends_joining = int(num_friends_joining)
         except ValueError:
             print("No one is joining for the party")
-            break
+            sys.exit()
 
-        if friends_joining < 1:
+        if num_friends_joining < 1:
             print("No one is joining for the party")
-            break
+            sys.exit()
 
-        list_friends_joining = []
+        return num_friends_joining
 
-        print('Enter the name of every friend (including you), each on a new line:')
-        for i in range(friends_joining):
-            list_friends_joining.append(input())
 
-        dict_friends_joining = dict.fromkeys(list_friends_joining, 0)
-        print(dict_friends_joining)
-        break
-take_input()
+def get_friends_names(num_friends):
+    print('Enter the name of every friend (including you), each on a new line:')
+    dict_friends_joining = {input(): 0 for _ in range(num_friends)}
+    return dict_friends_joining
+
+
+class BillSplitter:
+    def __init__(self):
+        self.num_friends = 0
+        self.final_bill = 0
+        self.friends = dict()
+        self.lucky = None
+
+    def take_input(self):
+        self.num_friends = get_num_friends()
+        print()
+        self.friends = get_friends_names(self.num_friends)
+        print()
+        print("Enter the total bill value:")
+        self.final_bill = float(input())
+        print()
+
+    def split_the_bill(self):
+        split_sum = self.final_bill / self.num_friends
+        split_sum = round(split_sum, 2)
+
+        for key in list(self.friends):
+            if key == self.lucky:
+                continue
+            self.friends[key] = split_sum
+
+        print(self.friends)
+
+    def pick_lucky_person(self):
+        print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+        answer = input()
+
+        if answer.lower() == "yes":
+            self.lucky = random.choice(list(self.friends))
+            print(f"\n{self.lucky} is the lucky one!\n")
+            self.num_friends -= 1
+        else:
+            print('No one is going to be lucky')
+
+
+def main():
+    new_bill = BillSplitter()
+    new_bill.take_input()
+    new_bill.pick_lucky_person()
+    new_bill.split_the_bill()
+
+
+if __name__ == '__main__':
+    main()
